63860448a6778b14d8c48defeb906d42cd79c01e
René Majland <rene@majland.de>
2026-04-24 09:28:30 +0200

    Delete main directory

--- main/java/dk/easv/weblager/be/Role.java
+++
@@ -1,9 +0,0 @@
-package dk.easv.weblager.be;
-
-/**
- * The role of a user. Determines which dashboard they see after login.
- */
-public enum Role {
-    ADMIN,
-    OPERATOR
-}
--- main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module dk.easv.weblager {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens dk.easv.weblager.gui         to javafx.fxml;
-    opens dk.easv.weblager.gui.control to javafx.fxml;
-
-    exports dk.easv.weblager;
-}
